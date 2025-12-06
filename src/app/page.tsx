46062ab050ffbcc68c77c82b2f6c8b5a7364b8cb
SaadiaAsghar <saadianigah@gmail.com>
2025-12-06 14:49:02 +0500

    Force re-deployment to apply critical fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { HowItWorks } from "@/components/home/HowItWorks";
 import { FeaturedEvents } from "@/components/home/FeaturedEvents";
 
 export default function Home() {
+    // Force re-deployment to ensure latest changes are live
     return (
         <main className="flex min-h-screen flex-col">
             <HeroSection />
